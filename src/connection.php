58dea7a2a9eb4bde229786fe7865119baeb6d72c
Ange627 <arnold_du_67@hotmail.fr>
2023-07-06 10:13:17 +0200

    Premier instance du projet

--- src/connection.php
+++ src/connection.php
@@ -1,5 +1,5 @@
  <?php
-$servername = "localhost"; // Server name where the app has the DB
+$servername = "SimpleAppBDD"; // Server name where the app has the DB
 $username = "root"; // user of the DB 
 $password = "root"; // password of the DB
 $dbname = "mono"; // DB name 
@@ -10,5 +10,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-?> 
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c953268755621422b3faa88fa5b49aba4f72827d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c953268755621422b3faa88fa5b49aba4f72827d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.10.24',
+            'version' => '1.10.24.0',
+            'reference' => '360ecc90569e9a60c2954ee209ec04fa0d958e14',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
